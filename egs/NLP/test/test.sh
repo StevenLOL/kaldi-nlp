572d3f09165c9bc52f89efeec26133d73ad17a70
dusijun <dusijun@gmail.com>
2016-04-08 15:49:42 +0800

    test speed

--- egs/NLP/test/test.sh
+++ egs/NLP/test/test.sh
@@ -22,6 +22,9 @@
 
 
 . path.sh
+
+###############Functional testing####################
+
 delta_opts="--delta-window=3 --delta-order=2"
 all_feats="ark,s,cs:add-deltas $delta_opts ark:./test.ark ark:- |"
 add-deltas $delta_opts ark:./test.ark ark,t: > output_add_delta.ark
@@ -37,3 +40,24 @@ embed-feats --w2vdim=20 ark:./dict.ark ark:./test-embed.ark ark:- | add-deltas $
 feat-to-dim ark:./output_add_delta_embed_feats.ark ark,t:- 
 feat-to-len ark:./output_add_delta_embed_feats.ark ark,t:-
 feat-to-dim ark:dict.ark ark,t: 
+
+#All files gives same output
+
+#############TEST SPEED################################
+#test w2v and load dict as ark or scp
+#load a large ark will no doubt slower than load an scp
+#./w2v.en.20.ark    		Google word2vector 20 dim 30M words, in raw text format  (640MB)
+#./w2v.en.20.kaldi.ark 	Google word2vector 20 dim 30M words, in binary format    (314MB)
+#./w2v.en20.kaldi.scp   scp file of above ark file                               (131MB)
+copy-vector ark:./w2v.en.20.ark ark,scp:./w2v.en.20.kaldi.ark,w2v.en20.kaldi.scp
+
+##slow## 40s
+time embed-feats --w2vdim=20 ark:./w2v.en.20.ark ark:./free_text.txt ark,t:-
+time embed-feats --w2vdim=20 ark:./w2v.en.20.ark ark:./free_text_10x.txt ark,t:-
+##faster## 10s
+time embed-feats --w2vdim=20 scp:./w2v.en.20.kaldi.scp ark:./free_text.txt ark,t:-
+time embed-feats --w2vdim=20 scp:./w2v.en.20.kaldi.scp ark:./free_text_10x.txt ark,t:-
+##faster+## 7s
+time embed-feats --w2vdim=20 ark:./w2v.en.20.kaldi.ark ark:./free_text.txt ark,t:-
+time embed-feats --w2vdim=20 ark:./w2v.en.20.kaldi.ark ark:./free_text_10x.txt ark,t:-
+
